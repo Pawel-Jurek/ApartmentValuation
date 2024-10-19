446f769cf68d9f120d248bfd9b7e742aa2a45388
Paweł J <pawel80121@gmail.com>
2024-10-19 19:14:10 +0200

    Database connection

--- ApartmentValuation/settings.py
+++ ApartmentValuation/settings.py
@@ -83,9 +83,14 @@ WSGI_APPLICATION = 'ApartmentValuation.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "houses-db",
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "apartament-valuation.postgres.database.azure.com",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
