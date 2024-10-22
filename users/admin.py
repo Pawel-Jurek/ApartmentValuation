445064ce62cc77e841b13d6c65fd81b6cab1291b
Paweł J <pawel80121@gmail.com>
2024-10-22 10:34:16 +0200

    improvement of user serializer and admin

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import User #, ApartmentSearch
+from .models import ApartmentSearch
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-# class ApartmentSearchAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'city', 'district', 'floor', 'rooms', 'square_meters', 'year', 'suggested_price_min', 'suggested_price_max', 'search_date')
-#     search_fields = ('user__username', 'city', 'district')
-#     list_filter = ('city', 'district', 'search_date')
+@admin.register(ApartmentSearch)
+class ApartmentSearchAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'district', 'floor', 'rooms', 'square_meters', 'year', 'suggested_price_min', 'suggested_price_max', 'search_date')
+    search_fields = ('user__username', 'city', 'district')
+    list_filter = ('city', 'district', 'search_date')
 
 
-# admin.site.register(ApartmentSearch, ApartmentSearchAdmin)
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
