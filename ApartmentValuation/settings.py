71d8e88e90457e3865a6a29bfa4d935468b07779
Paweł Jurek <pawel80121@gmail.com>
2024-12-04 11:27:34 +0100

    currentDate fixes

--- ApartmentValuation/settings.py
+++ ApartmentValuation/settings.py
@@ -63,7 +63,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 ROOT_URLCONF = 'ApartmentValuation.urls'
 
